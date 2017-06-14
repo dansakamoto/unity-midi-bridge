ab8068f10f738e66020468e0eb0dbf1b47e1bc43
Dan Sakamoto <contact@dansakamoto.com>
2017-06-14 16:53:13 -0400

    Cleaned up

--- Assets/Out/ControlLoop.cs
+++ Assets/Out/ControlLoop.cs
@@ -4,11 +4,8 @@ using System.Collections;
 public class ControlLoop : MonoBehaviour
 {
     public MidiChannel channel = MidiChannel.Ch1;
-    public int noteNumber = 48;
-    public float velocity = 0.9f;
     public float delay = 2.0f;
     public float length = 0.1f;
-    public float interval = 1.0f;
 
     float scale;
 
@@ -25,18 +22,12 @@ public class ControlLoop : MonoBehaviour
 			MidiOut.SendControlChange(channel, 2, (1+Mathf.Sin(Time.fixedTime))/2f);
 			scale = (1+Mathf.Sin(Time.fixedTime))/2f;
             yield return new WaitForSeconds (length);
-           // MidiOut.SendNoteOff (channel, noteNumber);
-            //yield return new WaitForSeconds (interval - length);
-
-
-
-
+		
         }
     }
 
     void Update ()
     {
-       // scale = 1.0f - (1.0f - scale) * Mathf.Exp (Time.deltaTime * -4.0f);
         transform.localScale = Vector3.one * scale;
     }
 }
